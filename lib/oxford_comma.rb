67ee2ce2426bfd68f425deb4a927fd2613985f1b
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-05 01:01:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last = array.pop
+    string = array.join(", ") + ", and " + last
+    string
+  end
 end
